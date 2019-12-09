a9adbdc536ae0b1ac1b54bc260b0761bbd8600d2
Clare Constantine <clare@code.org>
2019-12-08 23:22:51 -0800

    day 6 part 2

--- 2019/day_6_part_2.rb
+++ 2019/day_6_part_2.rb
@@ -1,24 +1,31 @@
 #
-# This is a solution for https://adventofcode.com/2019/day/6
+# This is a solution for https://adventofcode.com/2019/day/6#part2
 #
 
 class SpaceObject
   def initialize id
     @id = id
+    @orbited_obj = nil
     @orbited_by = []
     @num_orbits = nil
   end
 
-  # children
-  def orbited_by
-    @orbited_by
+  def id
+    @id
+  end
+
+  # parent
+  def orbited_obj
+    @orbited_obj
+  end
+
+  def set_orbited_obj space_object
+    @orbited_obj = space_object
   end
 
   def add_orbiter space_object
     @orbited_by.push space_object
   end
-
-
 
   def get_total_orbits
     if @orbited_by.empty?
@@ -54,6 +61,8 @@ class UniversalOrbitMap
       end
       # add orbiter as a child
       orbit_map[orbited_id].add_orbiter orbit_map[orbiter_id]
+      # add orbited object as a parent
+      orbit_map[orbiter_id].set_orbited_obj orbit_map[orbited_id]
     end
     @map = orbit_map
   end
@@ -75,6 +84,28 @@ map.each do |id, space_object|
 end
 
 puts "TOTAL: #{total_orbits}"
+
+# part 2
+
+you = map["YOU"]
+santa = map["SAN"]
+shared_orbit = nil
+
+you_to_com = []
+current_obj = you.orbited_obj
+while current_obj.orbited_obj
+  you_to_com.push current_obj.id
+  current_obj = current_obj.orbited_obj
+end
+
+santa_to_shared = []
+current_obj = santa.orbited_obj
+while !you_to_com.include? current_obj.id
+  santa_to_shared.push current_obj.id
+  current_obj = current_obj.orbited_obj
+end
+
+puts "TRANSFERS: #{you_to_com.find_index(current_obj.id) + santa_to_shared.length}"
 
 
 
