eb0b5ac2af261d495e2c59e1736ceec04d110243
Clare Constantine <clare.constantine@gmail.com>
2022-12-01 17:41:23 -0800

    2015 day 2, part 2

--- 2015/day_2.rb
+++ 2015/day_2.rb
@@ -8,16 +8,30 @@ def box_paper box
   face_2 = 2 * edge_2 * edge_3
   face_3 = 2 * edge_3 * edge_1
 
-  face_1 + face_2 + face_3 + edge_1 * edge_2
+  face_1 + face_2 + face_3 + (edge_1 * edge_2)
+end
+
+def box_ribbon box
+  sorted_dimensions = box.split('x').map(&:to_i).sort
+  edge_1 = sorted_dimensions[0]
+  edge_2 = sorted_dimensions[1]
+  edge_3 = sorted_dimensions[2]
+
+  (2 * edge_1) + (2 * edge_2) + (edge_1 * edge_2 * edge_3)
 end
 
 def main
   boxes = File.open("day_2_input.txt").readlines.map(&:chomp)
+
   total_paper = 0
+  total_ribbon = 0
+
   boxes.each do |box|
     total_paper += box_paper(box)
+    total_ribbon += box_ribbon(box)
   end
-  p total_paper
+  p "total paper: #{total_paper}"
+  p "total ribbon: #{total_ribbon}"
 end
 
 main
