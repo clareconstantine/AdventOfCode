263143380cad5cec60afced201d466897b3c8138
Clare Constantine <clare.constantine@gmail.com>
2022-12-01 17:15:44 -0800

    2015 day 1, part 2

--- 2015/day_1.rb
+++ 2015/day_1.rb
@@ -2,12 +2,14 @@ input = File.open("day_1_input.txt").readlines.map(&:chomp)[0]
 
 floor = 0
 
-input.each_char do |char|
+input.each_char.with_index do |char, i|
   if char == '('
     floor += 1
   elsif char == ')'
     floor -= 1
   end
+  if floor == -1
+    p i+1
+    break
+  end
 end
-
-p floor
