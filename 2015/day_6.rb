a4e3c34426f57279dac57ab76d757f2d735b144e
Clare Constantine <clare.constantine@gmail.com>
2022-12-05 13:06:41 -0800

    2015 day 6, part 2

--- 2015/day_6.rb
+++ 2015/day_6.rb
@@ -1,5 +1,4 @@
-def update_lights directions
-  lights = Array.new(1000, Array.new(1000, false))
+def update_lights directions, lights, switch_function
   directions.each do |dir|
     action = dir[0]
     x_start = dir[1].to_i
@@ -11,7 +10,7 @@ def update_lights directions
       if i >= y_start && i <= y_end # the row is in range
         light_row = light_row.each_with_index.map do |light, j|
           if j >= x_start && j <= x_end # the light is in range
-            light = switch(action, light)
+            light = method(switch_function).call(action, light)
           end
           light
         end
@@ -23,7 +22,7 @@ def update_lights directions
   lights
 end
 
-def switch action, light
+def on_off_switch action, light
   case action
   when "toggle"
     !light
@@ -34,7 +33,19 @@ def switch action, light
   end
 end
 
-def count_lights light_rows
+def brightness_switch action, light
+  case action
+  when "toggle"
+    light += 2
+  when "on"
+    light += 1
+  else
+    light > 0 ? light -= 1 : 0
+  end
+  light
+end
+
+def num_lights_on light_rows
   count = 0
   light_rows.each do |row|
     count += row.count(true)
@@ -42,13 +53,24 @@ def count_lights light_rows
   count
 end
 
+def total_brightness (light_rows)
+  total = 0
+  light_rows.each do |row|
+    total += row.sum
+  end
+  total
+end
+
 def part_1 directions
-  lights = update_lights(directions)
-  count_lights(lights)
+  lights = Array.new(1000, Array.new(1000, false))
+  lights = update_lights(directions, lights, :on_off_switch)
+  num_lights_on(lights)
 end
 
 def part_2 directions
-
+  lights = Array.new(1000, Array.new(1000, 0))
+  lights = update_lights(directions, lights, :brightness_switch)
+  total_brightness(lights)
 end
 
 def main
@@ -57,7 +79,7 @@ def main
   end
 
   p part_1(directions)
-  # p part_2(directions)
+  p part_2(directions)
 end
 
 main
