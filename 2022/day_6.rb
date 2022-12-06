db38adfefd06f3d9791655f9c24c2b10b4cb009f
Clare Constantine <clare.constantine@gmail.com>
2022-12-06 08:37:22 -0800

    2022 day 6

---
+++ 2022/day_6.rb
@@ -0,0 +1,30 @@
+def part_1 stream
+  find_marker(stream, 4)
+end
+
+def part_2 stream
+  find_marker(stream, 14)
+end
+
+def find_marker stream, marker_length
+  marker_candidate = []
+
+  stream.each_with_index do |char, i|
+    marker_candidate.push(char)
+    if i > marker_length - 1
+      marker_candidate.shift
+    end
+    if marker_candidate.uniq.length == marker_length
+      return i + 1
+    end
+  end
+end
+
+def main
+  stream = File.open("day_6_input.txt").readlines.map(&:chomp)[0].chars
+
+  p part_1(stream)
+  p part_2(stream)
+end
+
+main
