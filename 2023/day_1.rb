aaf6b931769ce3a3d0f5c2437bf4525d86fe97b9
Clare Constantine <clare.constantine@gmail.com>
2023-12-04 21:06:12 -0800

    2023 day 1 p1

--- 2023/day_1.rb
+++ 2023/day_1.rb
@@ -2,6 +2,16 @@ PATH_SNIPPET = "day_1"
 def main
   input = File.open("./input/#{PATH_SNIPPET}.txt").readlines.map(&:chomp)
 
+  total = 0
+  input.each do |line|
+    digits = line.scan /\d/
+    first_digit = digits.first.to_i
+    last_digit = digits.last.to_i
+
+    total += first_digit * 10 + last_digit
+  end
+
+  puts total
 end
 
 main
