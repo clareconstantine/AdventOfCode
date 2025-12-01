3d964691b696de9042063403dcf91facd9c039da
Clare Constantine <clare@binti.com>
2025-12-01 17:58:36 -0500

    day 1 part 1

---
+++ 2025/day_1.rb
@@ -0,0 +1,47 @@
+PATH_SNIPPET = "day_1"
+def main
+  # input = File.open("./input/test_input.txt").readlines.map(&:chomp)
+  input = File.open("./input/#{PATH_SNIPPET}.txt").readlines.map(&:chomp)
+
+  puts part_1(input)
+  puts part_2(input)
+end
+
+# Parts =========================================
+
+def part_1 input
+  rotations = input.map do |line|
+    direction = line[0]
+    distance = line[1..].to_i
+
+    if direction == "L"
+      distance *= -1
+    end
+
+    distance
+  end
+
+  dial = 50
+  pw = 0
+
+  rotations.each do |turn|
+    dial += turn
+    
+    dial = dial % 100
+    dial += 100 if dial < 0
+
+    pw += 1 if dial == 0
+  end
+
+  pw
+end
+
+def part_2 input
+ "xmas!"
+end
+
+# Helpers =======================================
+
+# ===============================================
+
+main
