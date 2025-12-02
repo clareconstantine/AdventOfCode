7d845324f675c6cdad8f3a3799c0657de5235a6b
Clare Constantine <clare@binti.com>
2025-12-01 19:41:28 -0500

    another part 2 attempt

--- 2025/day_1.rb
+++ 2025/day_1.rb
@@ -37,7 +37,35 @@ def part_1 input
 end
 
 def part_2 input
- "xmas!"
+  dial = 50
+  pw = 0
+
+  rotations = input.map do |line|
+    direction = line[0]
+    distance = line[1..].to_i
+
+    if direction == "L"
+      distance *= -1
+    end
+
+    distance
+  end
+
+  # [-49, -24, 48...]
+  rotations.each do |turn| 
+    dial += turn
+
+    pw += (dial / 100).abs
+
+    if dial < 0
+      dial = (-1 * (dial % 100)) + 100
+      pw += 1
+    else
+      dial = dial % 100
+    end
+  end
+
+  pw
 end
 
 # Helpers =======================================
